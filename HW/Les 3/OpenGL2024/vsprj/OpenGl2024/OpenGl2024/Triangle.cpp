a23ad68d149de6d510c4cf85b334d8bd4dcb2c65
Adam <Fireworksanimations21@gmail.com>
2024-07-20 17:11:37 +0200

    rommel

--- HW/Les 3/OpenGL2024/vsprj/OpenGl2024/OpenGl2024/Triangle.cpp
+++ HW/Les 3/OpenGL2024/vsprj/OpenGl2024/OpenGl2024/Triangle.cpp
@@ -1,5 +1,7 @@
 #include "Triangle.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -13,216 +15,300 @@ Triangle::~Triangle()
 }
 
 void Triangle::setupTextures() {
-	dirt = loadTexture("Textures/BlockText.jpg", 0);
-	//sand = loadTexture("Textures/sand.jpg", 0);
-	//grass = loadTexture("Textures/grass.png", 4);
-	//rock = loadTexture("Textures/rock.jpg", 0);
-	//snow = loadTexture("Textures/snow.jpg", 0);
+    dirt = loadTexture("Textures/BlockText.jpg", 0);
+    //sand = loadTexture("Textures/sand.jpg", 0);
+    //grass = loadTexture("Textures/grass.png", 4);
+    //rock = loadTexture("Textures/rock.jpg", 0);
+    //snow = loadTexture("Textures/snow.jpg", 0);
 }
 
 void Triangle::processUniforms(GLuint program) {
-	glUseProgram(program);
-	glUniform1i(glGetUniformLocation(program, "ourTexture"), 0);
-	//glUniform1i(glGetUniformLocation(program, "sand"), 1);
-	//glUniform1i(glGetUniformLocation(program, "grass"), 2);
-	//glUniform1i(glGetUniformLocation(program, "rock"), 3);
-	//glUniform1i(glGetUniformLocation(program, "snow"), 4);
+    glUseProgram(program);
+    glUniform1i(glGetUniformLocation(program, "ourTexture"), 0);
+    //glUniform1i(glGetUniformLocation(program, "sand"), 1);
+    //glUniform1i(glGetUniformLocation(program, "grass"), 2);
+    //glUniform1i(glGetUniformLocation(program, "rock"), 3);
+    //glUniform1i(glGetUniformLocation(program, "snow"), 4);
 }
 
 unsigned int Triangle::loadTexture(const std::string& url, int comp) {
-	unsigned int textureID;
-	glGenTextures(1, &textureID);
-	glBindTexture(GL_TEXTURE_2D, textureID);
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	int width, height, numChannels;
-	unsigned char* data = stbi_load(url.c_str(), &width, &height, &numChannels, comp);
+    int width, height, numChannels;
+    unsigned char* data = stbi_load(url.c_str(), &width, &height, &numChannels, comp);
 
-	if (data) {
-		std::cout << "Loaded texture data: " << static_cast<void*>(data) << std::endl;
-		std::cout << "Texture width: " << width << ", height: " << height << ", channels: " << numChannels << std::endl;
+    if (data) {
+        std::cout << "Loaded texture data: " << static_cast<void*>(data) << std::endl;
+        std::cout << "Texture width: " << width << ", height: " << height << ", channels: " << numChannels << std::endl;
 
-		if (comp != 0) numChannels = comp;
+        if (comp != 0) numChannels = comp;
 
-		switch (numChannels) {
-		case 3:
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-			break;
-		case 4:
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-			break;
-		default:
-			std::cerr << "Unsupported number of channels: " << numChannels << std::endl;
-			stbi_image_free(data);
-			return 0;
-		}
+        switch (numChannels) {
+        case 3:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            break;
+        case 4:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            break;
+        default:
+            std::cerr << "Unsupported number of channels: " << numChannels << std::endl;
+            stbi_image_free(data);
+            return 0;
+        }
 
-		GLenum error = glGetError();
-		if (error != GL_NO_ERROR) {
-			std::cerr << "Error uploading texture data to GPU: " << error << std::endl;
-		}
+        GLenum error = glGetError();
+        if (error != GL_NO_ERROR) {
+            std::cerr << "Error uploading texture data to GPU: " << error << std::endl;
+        }
 
-		glGenerateMipmap(GL_TEXTURE_2D);
-		error = glGetError();
-		if (error != GL_NO_ERROR) {
-			std::cerr << "Error generating mipmaps: " << error << std::endl;
-		}
+        glGenerateMipmap(GL_TEXTURE_2D);
+        error = glGetError();
+        if (error != GL_NO_ERROR) {
+            std::cerr << "Error generating mipmaps: " << error << std::endl;
+        }
 
-		std::cout << "Texture loaded successfully: " << url << std::endl;
-	}
-	else {
-		std::cerr << "Failed to load texture: " << url << std::endl;
-		return 0;
-	}
+        std::cout << "Texture loaded successfully: " << url << std::endl;
+    }
+    else {
+        std::cerr << "Failed to load texture: " << url << std::endl;
+        return 0;
+    }
 
-	stbi_image_free(data);
-	glBindTexture(GL_TEXTURE_2D, 0);
+    stbi_image_free(data);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
-	return textureID;
+    return textureID;
+}
+
+std::string Triangle::readFile(const std::string& filePath) {
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filePath << std::endl;
+        return "";
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
 }
 
 void Triangle::setupShaders()
 {
-	if (!glfwInit())
-		exit(EXIT_FAILURE);
+    glEnable(GL_DEPTH_TEST);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLuint vertex_buffer, index_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glGenBuffers(1, &index_buffer);
 
-	window = glfwCreateWindow(640, 480, "OpenGL Cube", NULL, NULL);
-	if (!window)
-	{
-		std::cerr << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 
-	glfwMakeContextCurrent(window);
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cerr << "Failed to initialize GLAD" << std::endl;
-		return;
-	}
-	glfwSwapInterval(1);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
 
-	glEnable(GL_DEPTH_TEST);
+    const std::string vertexShaderSource = readFile("simpleVertex.shader");
+    const std::string fragmentShaderSource = readFile("simpleFragment.shader");
 
-	GLuint vertex_buffer, index_buffer;
-	glGenBuffers(1, &vertex_buffer);
-	glGenBuffers(1, &index_buffer);
+    const char* vertex_shader_text = vertexShaderSource.c_str();
+    const char* fragment_shader_text = fragmentShaderSource.c_str();
 
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+    const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
+    GLint success;
+    GLchar infoLog[512];
 
-	const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-	glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-	GLint success;
-	GLchar infoLog[512];
+    const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
 
-	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
-		std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-	const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-	glCompileShader(fragment_shader);
+    const GLuint program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
 
-	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
-		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        std::cerr << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
 
-	const GLuint program = glCreateProgram();
-	glAttachShader(program, vertex_shader);
-	glAttachShader(program, fragment_shader);
-	glLinkProgram(program);
+    glUseProgram(program);
 
-	glGetProgramiv(program, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(program, 512, NULL, infoLog);
-		std::cerr << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	}
+    const GLint modelLoc = glGetUniformLocation(program, "model");
+    const GLint viewLoc = glGetUniformLocation(program, "view");
+    const GLint projectionLoc = glGetUniformLocation(program, "projection");
 
-	glUseProgram(program);
+    mat4x4 model, view, projection;
+    mat4x4_identity(model);
+    mat4x4_identity(view);
+    mat4x4_identity(projection);
 
-	const GLint mvp_location = glGetUniformLocation(program, "MVP");
-	const GLint vpos_location = glGetAttribLocation(program, "vPos");
-	const GLint vcol_location = glGetAttribLocation(program, "vCol");
-	const GLint vuv_location = glGetAttribLocation(program, "vUv");
+    mat4x4_translate_in_place(view, 0.0f, 0.0f, -3.0f);
 
-	GLuint vertex_array;
-	glGenVertexArrays(1, &vertex_array);
-	glBindVertexArray(vertex_array);
+    int width, height;
+    glfwGetFramebufferSize(glfwGetCurrentContext(), &width, &height);
+    float aspect = (float)width / (float)height;
+    mat4x4_perspective(projection, 1.0f, aspect, 0.1f, 100.0f);
 
-	int stride = (3 + 3 + 2 + 3) * sizeof(float);
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (const GLfloat*)&view);
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, (const GLfloat*)&projection);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-	glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
-	glEnableVertexAttribArray(vpos_location);
-	glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(vcol_location);
-	glVertexAttribPointer(vuv_location, 2, GL_FLOAT, GL_FALSE, stride, (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(vuv_location);
+    GLuint vertex_array;
+    glGenVertexArrays(1, &vertex_array);
+    glBindVertexArray(vertex_array);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    int stride = (3 + 3 + 2 + 3) * sizeof(float);
 
-	setupTextures();
-	processUniforms(program);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, stride, (void*)(8 * sizeof(float)));
+    glEnableVertexAttribArray(3);
 
-	while (!glfwWindowShouldClose(window))
-	{
-		int width, height;
-		glfwGetFramebufferSize(window, &width, &height);
-		const float ratio = width / (float)height;
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
 
-		processInput(window);
+    setupTextures();
 
-		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // Set light properties
+    glUniform3f(glGetUniformLocation(program, "light.position"), 1.2f, 10.0f, 2.0f);
+    glUniform3f(glGetUniformLocation(program, "light.ambient"), 1.0f, 1.0f, 1.0f);
+    glUniform3f(glGetUniformLocation(program, "light.diffuse"), 3.0f, 3.0f, 3.0f);
+    glUniform3f(glGetUniformLocation(program, "light.specular"), 1.0f, 1.0f, 1.0f);
 
-		mat4x4 m, p, mvp;
-		mat4x4_identity(m);
-		mat4x4_rotate_Y(m, m, (float)glfwGetTime());
-		mat4x4_perspective(p, 1.0f, ratio, 0.1f, 10.0f);
-		mat4x4_translate_in_place(p, 0.0f, 0.0f, -2.5f);
-		mat4x4_mul(mvp, p, m);
+    // Set material properties
+    glUniform3f(glGetUniformLocation(program, "material.ambient"), 1.0f, 1.0f, 1.0f);
+    glUniform3f(glGetUniformLocation(program, "material.diffuse"), 1.0f, 1.0f, 1.0f);
+    glUniform3f(glGetUniformLocation(program, "material.specular"), 1.0f, 1.0f, 1.0f);
+    glUniform1f(glGetUniformLocation(program, "material.shininess"), 32.0f);
 
-		glUseProgram(program);
-		glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)&mvp);
-		glBindVertexArray(vertex_array);
-		glBindTexture(GL_TEXTURE_2D, dirt); // Bind the texture before drawing
-		glDrawElements(GL_TRIANGLES, sizeof(cubeIndices) / sizeof(cubeIndices[0]), GL_UNSIGNED_INT, 0);
+    const float rotationSpeed = 1.0f; // Adjust rotation speed
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    //voor de rendering
+    const GLint timeLoc = glGetUniformLocation(program, "time");
 
-	glDeleteBuffers(1, &vertex_buffer);
-	glDeleteBuffers(1, &index_buffer);
-	glDeleteVertexArrays(1, &vertex_array);
-	glDeleteProgram(program);
-	glDeleteShader(vertex_shader);
-	glDeleteShader(fragment_shader);
+    while (!glfwWindowShouldClose(glfwGetCurrentContext())) {
+        int width, height;
+        glfwGetFramebufferSize(glfwGetCurrentContext(), &width, &height);
+        float aspect = (float)width / (float)height;
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
+        float timeValue = (float)glfwGetTime(); // Tijdvariabele voor animatie
+        glUniform1f(timeLoc, timeValue); // Stel de tijd uniform in
+
+
+        mat4x4_perspective(projection, 1.0f, aspect, 0.1f, 100.0f);
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, (const GLfloat*)&projection);
+
+        processInput(glfwGetCurrentContext());
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        float angle = (float)glfwGetTime() * rotationSpeed;
+        mat4x4_identity(model);
+        mat4x4_rotate_Y(model, model, angle);
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (const GLfloat*)&model);
+
+        glUseProgram(program);
+        glBindVertexArray(vertex_array);
+        glBindTexture(GL_TEXTURE_2D, dirt); // Bind the texture before drawing
+        glDrawElements(GL_TRIANGLES, sizeof(cubeIndices) / sizeof(cubeIndices[0]), GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(glfwGetCurrentContext());
+        glfwPollEvents();
+    }
+
+    glDeleteBuffers(1, &vertex_buffer);
+    glDeleteBuffers(1, &index_buffer);
+    glDeleteVertexArrays(1, &vertex_array);
+    glDeleteProgram(program);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    glfwDestroyWindow(glfwGetCurrentContext());
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
+
+void Triangle::render(GLFWwindow* window)
+{
+    if (window == NULL) {
+        std::cerr << "Window is NULL!" << std::endl;
+        return;
+    }
+
+    const GLint modelLoc = glGetUniformLocation(programID, "model");
+    const GLint timeLoc = glGetUniformLocation(programID, "time");
+
+
+
+    GLuint vertex_array;
+    glGenVertexArrays(1, &vertex_array);
+    glBindVertexArray(vertex_array);
+
+    const float rotationSpeed = 1.0f; // Adjust rotation speed
+
+    while (!glfwWindowShouldClose(window))
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        float aspect = (float)width / (float)height;
+
+        processInput(window);
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Bereken de rotatiehoek
+        float angle = (float)glfwGetTime() * rotationSpeed;
+        float timeValue = (float)glfwGetTime(); // Tijdvariabele voor animatie
+        std::cout << "test" << timeValue << std::endl;
+
+        mat4x4 model;
+        mat4x4_identity(model);
+        mat4x4_rotate_Y(model, model, angle);
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (const GLfloat*)&model);
+        glUniform1f(timeLoc, timeValue); // Stel de tijd uniform in
+
+        glUseProgram(programID);
+        glBindVertexArray(vertex_array);
+        glBindTexture(GL_TEXTURE_2D, dirt); // Bind de texture voor het tekenen
+        glDrawElements(GL_TRIANGLES, sizeof(cubeIndices) / sizeof(cubeIndices[0]), GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &vertex_array);
+}
+
+
 
 void Triangle::processInput(GLFWwindow* window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, true);
-	}
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
--- HW/Les 3/OpenGL2024/vsprj/OpenGl2024/OpenGl2024/main.cpp
+++ HW/Les 3/OpenGL2024/vsprj/OpenGl2024/OpenGl2024/main.cpp
@@ -6,36 +6,46 @@ int Init(GLFWwindow*& window);
 
 int main()
 {
-    Triangle myTriangle;
-    myTriangle.setupShaders();
+	GLFWwindow* window;
+	if (Init(window) != 0) {
+		return -1;
+	}
+
+	Triangle myTriangle;
+	myTriangle.setupShaders();
+	//myTriangle.render(window);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
 
+
 int Init(GLFWwindow*& window) {
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(resX, resY, "GLFW OpenGL3 Test Demo", NULL, NULL);
-    if (!window)
-    {
-        std::cout << "FAILED to init window" << std::endl;
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-        return -1;
-    }
+	window = glfwCreateWindow(resX, resY, "GLFW OpenGL3 Test Demo", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "FAILED to init window" << std::endl;
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+		return -1;
+	}
 
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
-    glfwSwapInterval(1);
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "failed to init GLAD" << std::endl;
-        return -1;
-    }
+	glfwSwapInterval(1);
 
-    return 0;
+	return 0;
 }
